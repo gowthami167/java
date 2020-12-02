520ff35ab3103e7c999332ddc0da63ce9e2e44ac
gowthami167 <69572396+gowthami167@users.noreply.github.com>
2020-12-02 11:03:52 +0530

    Add files via upload

---
+++ lab10.java
@@ -0,0 +1,20 @@
+class lab10
+{
+ static int count=0;
+  void incr()
+ {
+  count++;
+ }
+ public static void main(String args[])
+ {
+ lab10 c1=new lab10();
+ c1.incr();
+ lab10 c2=new lab10();
+ c2.incr();
+ lab10 c3=new lab10();
+ c3.incr();
+ lab10 c4=new lab10();
+ c4.incr();
+ System.out.println("number of objects="+count);
+ }
+}
